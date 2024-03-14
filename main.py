26d7eabbe22f5cb2ee8ea93e3057ab18cd5f95df
Fabián Doñaque <fabian.donaque@fabsrobotics.com>
2024-03-14 13:36:49 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+import cv2
+import numpy as np
+from pyzbar.pyzbar import decode
+
+cap = cv2.VideoCapture(1)
+cap.set(3,640)
+cap.set(4,480)
+
+while True:
+	success, img = cap.read()
+	for barcode in decode(img):
+		print(barcode.data)
+		myData = barcode.data.decode('utf-8')
+		print(myData)
+
+	cv2.imshow('Result',img)
+	cv2.waitKey(1)
+
+ 
